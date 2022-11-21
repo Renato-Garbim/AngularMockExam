ee355d0af06154378445080819a41f55ae064f3a
Renato Garbim <renatobordinigarbim@hotmail.com>
2022-11-21 12:08:43 -0300

    remove ngrx books

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,8 +3,6 @@ import { Store } from '@ngrx/store';
 import { Hero } from '../Entidades/Hero';
 import { BookService } from '../services/book.service';
 import { HeroService } from '../services/hero.service';
-import { addBook, removeBook, retrievedBookList } from '../state/books.action';
-import { selectBookCollection, selectBooks } from '../state/books.selector';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,17 +12,12 @@ import { selectBookCollection, selectBooks } from '../state/books.selector';
 export class DashboardComponent implements OnInit {
 
   heroes: Hero[] = [];
-  books$ = this.store.select(selectBooks);
-  bookCollection$ = this.store.select(selectBookCollection);
 
   constructor(private heroService: HeroService, private booksService: BookService, private store: Store) { }
 
   ngOnInit(): void {
     this.getHeroes();
     
-    this.booksService
-    .getBooks()
-    .subscribe((books) => this.store.dispatch(retrievedBookList({ books })));
   }
 
 
@@ -33,13 +26,6 @@ export class DashboardComponent implements OnInit {
     .subscribe(x => this.heroes = x.slice(0, 4));
   }
 
-  onAdd(bookId: string) {
-    this.store.dispatch(addBook({ bookId }));
-  }
-
-  onRemove(bookId: string) {
-    this.store.dispatch(removeBook({ bookId }));
-  }
 
 
 }
