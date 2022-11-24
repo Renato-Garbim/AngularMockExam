5f8b9135d04930d53b396e529f9548d0320429ab
Renato Garbim <renatobordinigarbim@hotmail.com>
2022-11-23 22:11:51 -0300

    effects implemetation

--- src/app/states/herostates/hero-api.actions.ts
+++ src/app/states/herostates/hero-api.actions.ts
@@ -5,3 +5,8 @@ export const getAllHeroes = createAction (
     '[Heroes API] get all heroes ', 
     props<{ heroes:  Hero[]}>()
 )
+
+export const DeleteHeroAPI = createAction (
+    '[Hero API] Delete Hero',
+     props<{heroId: string}>()
+)
--- src/app/states/herostates/hero-api.effects.ts
+++ src/app/states/herostates/hero-api.effects.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { map, mergeMap } from "rxjs";
 import { HeroService } from "src/app/services/hero.service";
-import { getAllHeroes } from "./hero-api.actions";
+import { DeleteHeroAPI, getAllHeroes } from "./hero-api.actions";
 import { enter } from "./hero-page-actions";
 
 @Injectable()
@@ -20,11 +20,10 @@ constructor(private heroService: HeroService, private actions$: Actions){}
                 .getAll()
                 .pipe(map((heroes) => getAllHeroes({heroes: heroes}) ))
             })
-
         )
         
     })
 
-    
+
 
 }
