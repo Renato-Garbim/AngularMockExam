c6eefc2effc31f77c49c5c310561f457a9676ec9
Renato Garbim <renatobordinigarbim@hotmail.com>
2022-12-20 09:18:54 -0300

    styles update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  content: ["./src/**/*.{html,js, ts}"],
+  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#202225',
+        secondary: '#5865f2'
+      }
+    },
   },
   plugins: [],
 }
